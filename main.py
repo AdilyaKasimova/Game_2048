fd526e6320a12f9b9e0de5af209e443da807476b
AdilyaKasimova <adalkasimova@edu.hse.ru>
2023-12-04 21:55:04 +0300

    Wrote basic functions and tests for them

--- main.py
+++ main.py
@@ -1,1 +1,50 @@
+import random
 
+
+def pretty_print(mas):
+    print('-' * 10)
+    for row in mas:
+        print(*row)
+    print('-' * 10)
+
+
+def get_number_from_index(i, j):  # определяет номер ячейки по ее индексам
+    return i * 4 + j + 1
+
+
+def get_index_from_number(num):  # определяет индексы ячейки по ее номеру
+    num -= 1
+    x, y = num // 4, num % 4
+    return x, y
+
+
+def get_empty_list(mas):  # составляет список с номерами пустых ячеек
+    empty = []
+    for i in range(4):
+        for j in range(4):
+            if mas[i][j] == 0:
+                num = get_number_from_index(i, j)
+                empty.append(num)
+    return empty
+
+
+def insert_2_or_4(mas, x, y): #заполнение ячеек 2 (вероятность 75%) и 4 (вероятность 25%)
+    if random.random() <= 0.75:
+        mas[x][y] = 2
+    else:
+        mas[x][y] = 4
+    return mas
+
+
+mas = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
+]
+mas[0][3] = 2
+mas[1][1] = 2
+mas[2][0] = 2
+mas[3][2] = 2
+print(get_empty_list(mas))
+pretty_print(mas)
--- main_test.py
+++ main_test.py
@@ -1,1 +1,44 @@
+import unittest
+from main import get_number_from_index, get_empty_list, get_index_from_number
 
+
+class Test_2048(unittest.TestCase):
+    def test_get_number_from_index(self):
+        self.assertEqual(get_number_from_index(3, 3), 16)
+        self.assertEqual(get_number_from_index(0, 0), 1)
+        self.assertEqual(get_number_from_index(1, 2), 7)
+
+    def test_get_empty_list(self):
+        a = [1, 2, 3, 5, 7, 8, 10, 11, 12, 13, 14, 16]
+        mas = [
+            [0, 0, 0, 2],
+            [0, 2, 0, 0],
+            [2, 0, 0, 0],
+            [0, 0, 2, 0]
+        ]
+        self.assertEqual(get_empty_list(mas), a)
+        a = []
+        mas = [
+            [2, 2, 2, 2],
+            [2, 2, 2, 2],
+            [2, 2, 2, 2],
+            [2, 2, 2, 2]
+        ]
+        self.assertEqual(get_empty_list(mas), a)
+        a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+        mas = [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ]
+        self.assertEqual(get_empty_list(mas), a)
+
+    def test_get_index_from_number(self):
+        self.assertEqual(get_index_from_number(1), (0, 0))
+        self.assertEqual(get_index_from_number(16), (3, 3))
+        self.assertEqual(get_index_from_number(5), (1, 0))
+
+
+if __name__ == 'main':
+    unittest.main()
